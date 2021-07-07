0c769d1b2ce0dca2c775416271cc0bb9d166a54c
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2021-07-07 20:36:21 +1200

    programme to figure out nz tax depending on salary

---
+++ sallary.py
@@ -0,0 +1,24 @@
+def calculate_tax(wages):
+    
+    if wages <= 14000:
+        wage_tax = .105
+    elif wages <= 48000:
+        wage_tax = .175
+    elif wages <= 70000:
+        wage_tax = .30
+    else:
+        wage_tax = .33
+
+    return wages * wage_tax
+
+print(calculate_tax(48000))
+
+
+
+
+
+
+
+
+
+
